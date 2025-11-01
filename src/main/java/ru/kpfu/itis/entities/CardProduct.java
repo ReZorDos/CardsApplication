8ee881a1d7a90c7061f504e6ef33c079f1ab18b3
Turbin <dickover@mail.ru>
2025-11-01 09:52:09 +0300

    add entities

---
+++ src/main/java/ru/kpfu/itis/entities/CardProduct.java
@@ -0,0 +1,21 @@
+package ru.kpfu.itis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CardProduct {
+    
+    private UUID id;
+    private String cardName;
+    private String description;
+    private String cardImageLink;
+}
+
