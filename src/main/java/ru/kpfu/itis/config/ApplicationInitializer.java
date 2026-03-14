da74ed50376d4f8c7ee49313eb3a2660d59e2328
ReZorDoc <bul.strint64@gmail.com>
2026-03-14 10:48:42 +0300

    add spring mvc

---
+++ src/main/java/ru/kpfu/itis/config/ApplicationInitializer.java
@@ -0,0 +1,24 @@
+package ru.kpfu.itis.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+
+        ContextLoaderListener contextLoaderListener = new ContextLoaderListener(applicationContext);
+        servletContext.addListener(contextLoaderListener);
+
+        ServletRegistration.Dynamic dispatcherServlet = servletContext.addServlet("dispatcher",
+                new DispatcherServlet(applicationContext));
+        dispatcherServlet.setLoadOnStartup(1);
+        dispatcherServlet.addMapping("/");
+    }
+}
